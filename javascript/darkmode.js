cc386e9f0a87cbec5f02d03fec2d9381146c98f3
Ellen Dahlgren <ellensofiadahlgren@outlook.com>
2022-11-30 12:16:33 +0100

    add icons and update darkmode.js

--- javascript/darkmode.js
+++ javascript/darkmode.js
@@ -1,12 +1,24 @@
 function darkmode() {
 
-    console.log("darkmode active"); 
     const body = document.querySelector('body');
     const darkmodeBtn = document.querySelector('.darkmode');
+
+    const linkedinIcon = document.querySelector('.contact__linkedin');
+    const mailIcon = document.querySelector('.contact__mail');
+    const phoneIcon = document.querySelector('.contact__phone');
+
     if (body.classList == 'darkmode-active') {
         darkmodeBtn.src = 'assets/icons/darkmode.svg';
+        linkedinIcon.src = 'assets/icons/linkedin.svg';
+        mailIcon.src = 'assets/icons/mail.svg';
+        phoneIcon.src = 'assets/icons/phone.svg';
+
     } else {
         darkmodeBtn.src = 'assets/icons/lightmode.svg';
+        
+        linkedinIcon.src = 'assets/icons/linkedin-white.svg';
+        mailIcon.src = 'assets/icons/mail-white.svg';
+        phoneIcon.src = 'assets/icons/phone-white.svg';
     }
     body.classList.toggle('darkmode-active');
     
