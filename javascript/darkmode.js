cadd4160082f01b240ccaaca7e828faa804881b8
Ellen Dahlgren <ellensofiadahlgren@outlook.com>
2022-12-01 12:00:40 +0100

    change default to darkmode and position of image

--- javascript/darkmode.js
+++ javascript/darkmode.js
@@ -2,6 +2,7 @@ function darkmode() {
 
     const body = document.querySelector('body');
     const darkmodeBtn = document.querySelector('.darkmode');
+    const github = document.querySelector('.header__github-icon');
 
     const linkedinIcon = document.querySelector('.contact__linkedin');
     const mailIcon = document.querySelector('.contact__mail');
@@ -12,6 +13,7 @@ function darkmode() {
         linkedinIcon.src = 'assets/icons/linkedin.svg';
         mailIcon.src = 'assets/icons/mail.svg';
         phoneIcon.src = 'assets/icons/phone.svg';
+        github.src = 'assets/icons/github.svg';
 
     } else {
         darkmodeBtn.src = 'assets/icons/lightmode.svg';
@@ -19,6 +21,7 @@ function darkmode() {
         linkedinIcon.src = 'assets/icons/linkedin-white.svg';
         mailIcon.src = 'assets/icons/mail-white.svg';
         phoneIcon.src = 'assets/icons/phone-white.svg';
+        github.src = 'assets/icons/github-white.svg';
     }
     body.classList.toggle('darkmode-active');
     
